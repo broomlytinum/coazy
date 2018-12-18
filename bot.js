02869ab28f581c031294c5d4037d56663b55b751
broomlytinum <velvetgems@gmail.com>
2018-12-18 13:21:23 -0500

    debugging

--- bot.js
+++ bot.js
@@ -44,6 +44,8 @@ function get_messages(collector, channel, start_date, limit, before=null) {
 
 			messages.forEach(message => {
 				if (message.createdAt.getTime() < start_date.getTime()) {
+					console.log(message.createdAt.getTime());
+					console.log(start_date.getTime());
 					finished = true;
 				} else {
 					earliest_message = message;
