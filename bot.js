49281c99f03baba3ec3daafc183a34ba1742e460
broomlytinum <velvetgems@gmail.com>
2018-12-17 21:10:40 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+var https = require("https");
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const bodyParser = require("body-parser");
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.use(bodyParser.urlencoded({extended: true}))
+	app.use(bodyParser.json());
+
+	app.use("/api/discord", require("./api/discord"));
+
+	const path = require("path");
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "index.html"));
+	});
+}
+
+app.listen(process.env.PORT || 8080);
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+});
+
+client.login(process.env.BOT_TOKEN);
